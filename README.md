a264fe67cc86e85d7b12bb29bc8e5db2413aecc2
De Luca Riccardo <xdelu02@gmail.com>
2021-03-31 08:16:17 +0200

    del

--- README.md
+++
@@ -1,3 +0,0 @@
-# ManageEmployees
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.6.
--- src/app/Employee.ts
+++
@@ -1,7 +0,0 @@
-export default class Employee {
-    employeeId: number;
-    firstName: string;
-    lastName: string;
-    email: string;
-    phone: string;
-}
